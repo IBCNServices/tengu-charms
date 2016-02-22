bb24e26be2fa6ed1d3f78cf99b0bdb5d41f686cf
Merlijn Sebrechts <merlijn.sebrechts@gmail.com>
2016-02-22 15:57:54 +0100

    fixes

--- charms/interfaces/opened-ports/requires.py
+++ charms/interfaces/opened-ports/requires.py
@@ -1,8 +1,15 @@
-#pylint:disable=c0111
+#python3 pylint:disable=c0111
+import json
+
+from charmhelpers.core import unitdata
+
 from charms.reactive import hook
 from charms.reactive import RelationBase
 from charms.reactive import scopes
 
+RANGE = 29000
+
+KV = unitdata.kv()
 
 class OpenedPortsRequires(RelationBase):
     scope = scopes.UNIT
@@ -13,7 +20,30 @@ class OpenedPortsRequires(RelationBase):
         if conv.get_remote('opened-ports'):
             # this unit's conversation has a port, so
             # it is part of the set of available units
+            opened_ports = json.loads(conv.get_remote('opened-ports'))
+            port_forwards = conv.get_local('port-forwards', [])
+            for portproto in opened_ports:
+                if not any(
+                        (pf['private_port'] == portproto['port']) and
+                        (pf['protocol'] == portproto['protocol'])
+                        for pf in port_forwards):
+                    free_port = KV.get('freeport', RANGE)
+                    KV.set('freeport', free_port + 1)
+                    port_forward = {
+                        "public_port": free_port,
+                        "private_port": portproto['port'],
+                        "private_ip": conv.get_remote('private-address'),
+                        "protocol": portproto['protocol'],
+                    }
+                    port_forwards.append(port_forward)
+                    conv.set_local('port-forwards', port_forwards)
+                    conv.set_remote('port-forwards', json.dumps(port_forwards))
             conv.set_state('{relation_name}.available')
+        # ping remote to rerun if requested
+        if conv.get_remote('pingpong', '') != '':
+            conv.set_remote('pingpong', self.get_remote('pingpong'))
+        else:
+            conv.set_remote('pingpong', '')
 
 
     @hook('{requires:opened-ports}-relation-{departed,broken}')
@@ -24,19 +54,20 @@ class OpenedPortsRequires(RelationBase):
 
     @property
     def opened_ports(self):
-        """ Returns list of opened [port, proto] pairs. """
-        services = {}
+        """ Returns list of dicts:  {
+            "public_port": "<public-port>",
+            "private_port": "<private_port>",
+            "private_ip": "<private_ip>",
+            "protocol": "<tcp/udp>"
+        } . """
+        services = []
         for conv in self.conversations():
-            output = conv.get_remote('opened-ports')
-            ports = []
-            if output:
-                for line in output.split('\n'):
-                    port, proto = line.split('/')
-                    if '-' in port:
-                        first, last = port.split('-')
-                        for port in range(first, last):
-                            ports.append((port, proto))
-                    else:
-                        ports.append((port, proto))
-            services[conv.get_remote('private-address')] = ports
+            port_forwards = conv.get_local('port-forwards', [])
+            services.extend(port_forwards)
         return services
+
+
+    def set_ready(self):
+        """ send a notice to the related charms that the port forwarding has been applied """
+        for conv in self.conversations():
+            conv.set_remote('ready', conv.get_remote('opened-ports', '[]'))
--- charms/layers/juju-client/templates/juju.sh
+++ charms/layers/juju-client/templates/juju.sh
@@ -1,3 +1,4 @@
 export JUJU_REPOSITORY={{charm_repo_path}}
-export COMPOSER_PATH={{charm_repo_path}}/layers
+export COMPOSER_PATH={{charm_repo_path}}/layers   # Deprecated
+export LAYER_PATH={{charm_repo_path}}/layers
 export INTERFACE_PATH={{charm_repo_path}}/interfaces

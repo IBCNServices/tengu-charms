d483d6a3b8e822da2b1900e55debca7a69f47464
Merlijn Sebrechts <merlijn.sebrechts@gmail.com>
2016-04-03 20:12:17 -0700

    fixes

--- charms/interfaces/oozie/requires.py
+++ charms/interfaces/oozie/requires.py
@@ -4,30 +4,32 @@ from charms.reactive import RelationBase
 from charms.reactive import scopes
 
 
-
 class OozieRequires(RelationBase):
     scope = scopes.GLOBAL
 
-    @hook('{requires:opened-ports}-relation-{joined,changed}')
+    @hook('{requires:oozie}-relation-{joined,changed}')
     def changed(self):
-        conv = self.conversation()
-        if conv.get_remote('private-address'):
+        if self.get_remote('private-address'):
+            conv = self.conversation()
             # TODO: Implement new relationship protocol that indicates when oozie is ready
+            conv.remove_state('{relation_name}.joined')
             conv.set_state('{relation_name}.ready')
 
 
-    @hook('{requires:opened-ports}-relation-{departed,broken}')
+    @hook('{requires:oozie}-relation-{departed,broken}')
     def broken(self):
         conv = self.conversation()
-        conv.remove_state('{relation_name}.available')
+        conv.remove_state('{relation_name}.ready')
+        conv.remove_state('{relation_name}.joined')
 
 
     @property
     def port(self):
         """ Return Oozie port"""
-        conv = self.conversation()
         # TODO: implement new relationship protocol that sends port number
+        conv = self.conversation()
         return conv.get_remote('port', str(11000))
+
 
     @property
     def private_address(self):

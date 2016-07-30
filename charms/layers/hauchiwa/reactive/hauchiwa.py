d32582282228cfea327bd7ee4ac832ffc30c844f
Merlijn Sebrechts <merlijn.sebrechts@gmail.com>
2016-07-30 22:32:40 +0200

    better logging if pip fails

--- charms/layers/hauchiwa/reactive/hauchiwa.py
+++ charms/layers/hauchiwa/reactive/hauchiwa.py
@@ -170,7 +170,7 @@ def install_tengu():
     """ Installs tengu management tools """
     packages = ['python-pip', 'tree', 'python-dev', 'unzip', 'make']
     fetch.apt_install(fetch.filter_installed_packages(packages))
-    subprocess.check_output(['pip2', 'install', 'Jinja2', 'Flask', 'pyyaml', 'click', 'python-dateutil', 'oauth2client', 'cloud-weather-report'])
+    subprocess.check_call(['pip2', 'install', 'Jinja2', 'Flask', 'pyyaml', 'click', 'python-dateutil', 'oauth2client', 'cloud-weather-report'])
     # Install Tengu. Existing /etc files don't get overwritten.
     t_dir = None
     if os.path.isdir(TENGU_DIR + '/etc'):

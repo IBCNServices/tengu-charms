48626245bfa3314084b60b1954fcdc0d843a98d4
Merlijn Sebrechts <merlijn.sebrechts@gmail.com>
2016-11-23 15:45:15 +0100

    fixes

--- charms/xenial/sojobo-api/files/sojobo-api/sbin/install_credentials.py
+++ charms/xenial/sojobo-api/files/sojobo-api/sbin/install_credentials.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #pylint: disable=c0111
-from os.path import expanduser, dirname, realpath
+from os.path import expanduser, dirname, realpath, isfile
 from shutil import copy2
 import subprocess
 import time
@@ -8,7 +8,7 @@ import time
 import yaml   # Should be present after installing Juju
 
 def init():
-    subprocess.check_call(['juju'])  # to make sure config folder is present
+    subprocess.check_output(['juju'])  # to make sure config folder is present
 
 def merge(a, b):#pylint: disable=c0103
     with open(b, 'r') as b_file:
@@ -70,17 +70,31 @@ def deep_merge(a, b):#pylint: disable=c0103
                            into "{}"'.format(e, key, b, a))
     return a
 
+MESSAGE = """\nConfig was succesfully added! You can now login using `juju login`.
+Note: if this isn't the first controller you're logging into; logout with `juju logout`, switch controller with `juju switch <controllername>`. You see the list of configured controllers with `juju list-controllers`.
+
+Next steps:
+ - See existing models with `juju list-models`.
+ - Make a model active with `juju switch <modelname>` and show the status with `juju status`.
+ - Add a new model with `juju add-model <new-modelname> --credential <username>`.
+ - Go to Juju's UI with `juju ui`.
+"""
+
 if __name__ == '__main__':
     init()
     timestamp = int(time.time())
-    copy2(expanduser('~/.local/share/juju/clouds.yaml'),
-          expanduser('~/.local/share/juju/clouds.yaml.bak{}'.format(timestamp)))
-    copy2(expanduser('~/.local/share/juju/credentials.yaml'),
-          expanduser('~/.local/share/juju/credentials.yaml.bak{}'.format(timestamp)))
-    copy2(expanduser('~/.local/share/juju/controllers.yaml'),
-          expanduser('~/.local/share/juju/controllers.yaml.bak{}'.format(timestamp)))
+    if isfile(expanduser('~/.local/share/juju/clouds.yaml')):
+        copy2(expanduser('~/.local/share/juju/clouds.yaml'),
+              expanduser('~/.local/share/juju/clouds.yaml.bak{}'.format(timestamp)))
+    if isfile(expanduser('~/.local/share/juju/credentials.yaml')):
+        copy2(expanduser('~/.local/share/juju/credentials.yaml'),
+              expanduser('~/.local/share/juju/credentials.yaml.bak{}'.format(timestamp)))
+    if isfile(expanduser('~/.local/share/juju/controllers.yaml')):
+        copy2(expanduser('~/.local/share/juju/controllers.yaml'),
+              expanduser('~/.local/share/juju/controllers.yaml.bak{}'.format(timestamp)))
     merge(expanduser('~/.local/share/juju/clouds.yaml'), 'clouds.yaml')
     merge(expanduser('~/.local/share/juju/credentials.yaml'),
           '{}/credentials.yaml'.format(dirname(realpath(__file__))))
     merge(expanduser('~/.local/share/juju/controllers.yaml'),
           '{}/controllers.yaml'.format(dirname(realpath(__file__))))
+    print(MESSAGE)
